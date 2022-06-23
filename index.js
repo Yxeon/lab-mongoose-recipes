aa10621d41b71e9d59b88a0f914a471caa59fe73
Angel Tejada <angelfernan018@gmail.com>
2022-06-23 20:20:44 +0200

    Recipe.model.js done (iteration 1)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
